97e08cf554621ac9ebce26d005e11e1422031697
alibenD <aliben.develop@gmail.com>
2018-08-16 09:23:15 +0800

    Added test file - init

---
+++ test/test_sophus.cpp
@@ -0,0 +1,25 @@
+/**-----------------------------------------------
+  * @Copyright (C) 2018 All rights reserved.
+  * @date: 2018
+  * @file: test_sophus.cpp
+  * @version: v0.0.1
+  * @author: aliben.develop@gmail.com
+  * @create_date: 2018-08-16 09:21:01
+  * @last_modified_date: 2018-08-16 09:22:11
+  * @brief: TODO
+  * @details: TODO
+  *-----------------------------------------------*/
+
+//INCLUDE
+#include <sophus/so2.hpp>
+#include <sophus/so3.hpp>
+#include <sophus/se2.hpp>
+#include <sophus/se3.hpp>
+#include <iostream>
+
+//CODE
+int main()
+{
+  std::cout << "This is a test for sophus" << std::endl;
+  return 0;
+}
