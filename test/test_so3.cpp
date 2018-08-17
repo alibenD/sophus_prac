a32b3e2613819609916265554a65929c45fd9b57
alibenD <aliben.develop@gmail.com>
2018-08-17 14:28:31 +0800

    Removed unrelated header for test_so3.cpp

--- test/test_so3.cpp
+++ test/test_so3.cpp
@@ -5,16 +5,13 @@
   * @version: v0.0.1
   * @author: aliben.develop@gmail.com
   * @create_date: 2018-08-16 09:21:01
-  * @last_modified_date: 2018-08-17 14:10:45
+  * @last_modified_date: 2018-08-17 14:26:48
   * @brief: TODO
   * @details: TODO
   *-----------------------------------------------*/
 
 //INCLUDE
-#include <sophus/so2.hpp>
 #include <sophus/so3.hpp>
-#include <sophus/se2.hpp>
-#include <sophus/se3.hpp>
 #include <Eigen/Core>
 #include <Eigen/Geometry>
 #include <iostream>
